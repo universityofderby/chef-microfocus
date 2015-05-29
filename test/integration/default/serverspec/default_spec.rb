89af9e29fbb5f4457c500613e105a2f24952fa1e
Richard Barkley <r.barkley@derby.ac.uk>
2015-05-29 16:21:46 +0100

    Fix broken tests - Add tests for libgcc

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -10,13 +10,13 @@ describe file('/opt/microfocus/mflmf') do
   it { should be_a_directory }
 end
 # cobopt & cobopt64
-describe file('/opt/microfocus/etc/cobopt') do
-  it { should be_a_directory }
+describe file('/opt/microfocus/cobol/etc/cobopt') do
+  it { should be_a_file }
   it { should be_mode 444 }
 end
 
-describe file('/opt/microfocus/etc/cobopt64') do
-  it { should be_a_directory }
+describe file('/opt/microfocus/cobol/etc/cobopt64') do
+  it { should be_a_file }
   it { should be_mode 444 }
 end
 
@@ -28,4 +28,14 @@ end
 # Inittab
 describe file('/etc/inittab') do
   it { should contain '/etc/mflmrcscript' }
+end
+
+# libgcc
+describe package('libgcc') do
+  it { should be_installed }
+end
+
+#libgcc.i686
+describe package('libgcc.i686') do
+  it { should be_installed }
 end
