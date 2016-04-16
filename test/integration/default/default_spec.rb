61b61228f421681ba21d4c9a4c1fde42fbc11b15
Richard Lock <r.j.lock@derby.ac.uk>
2016-04-16 22:29:43 +0100

    Added InSpec tests

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,18 +1,50 @@
-%w(gcc glibc libgcc).each do |p|
+# packages
+%w(gcc glibc glibc.i686 libgcc libgcc.i686).each do |p|
   describe package(p) do
     it { should be_installed }
   end
 end
 
-describe file('/opt/microfocus') do
-  it { should be_directory }
+# microfocus directories
+{ '/opt/microfocus' => 0755,
+  '/opt/microfocus/cobol' => 0755,
+  '/opt/microfocus/mflmf' => 0700
+}.each do |f, m|
+  describe file(f) do
+    it { should be_directory }
+    its(:owner) { should eq 'root' }
+    its(:group) { should eq 'root' }
+    its(:mode) { should eq m }
+  end
+end
+
+# microfocus files
+{ '/opt/microfocus/cobol/install' => 0555,
+  '/opt/microfocus/mflmf/mflmcmd' => 0555,
+  '/opt/microfocus/mflmf/mflm_manager' => 0544
+}.each do |f, m|
+  describe file(f) do
+    its(:owner) { should eq 'root' }
+    its(:group) { should eq 'root' }
+    its(:mode) { should eq m }
+  end
+end
+
+# license manager init.d script
+describe file('/etc/rc.d/init.d/mflm_manager') do
+  its(:content) { should match(%r{sh \/etc\/mflmrcscript > \/dev\/null 2>&1}) }
   its(:owner) { should eq 'root' }
   its(:group) { should eq 'root' }
   its(:mode) { should eq 0755 }
 end
 
-describe file('/opt/microfocus/cobol/install') do
-  its(:owner) { should eq 'root' }
-  its(:group) { should eq 'root' }
-  its(:mode) { should eq 0555 }
+# license manager service
+describe service('mflm_manager') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+# license manager process
+describe process('./mflm_manager') do
+  it { should be_running }
 end
